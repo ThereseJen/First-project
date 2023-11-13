776d626680247d4ae041591159bde7fcd6285780
ThereseJen <141225404+ThereseJen@users.noreply.github.com>
2023-11-13 14:53:15 +0100

    Litt mer moodle osv

--- Program.cs
+++ Program.cs
@@ -4,10 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hei, hva heter du?");
-            var userInput = Console.ReadLine();
-            Console.WriteLine($"Velkommen {userInput}!");
-        
+          /*Console.WriteLine("Hei, hva heter du?");
+          var userInput = Console.ReadLine();
+          Console.WriteLine($"Velkommen {userInput}!"); 
+
+           int tall = 7;
+           long langtTall = 3243424;
+           float floater = 11.8F;
+           decimal desimaltall = 11.8M;
+           double dobbeltall = 11.8;
+           string tekst = "Dette er en tekst.";
+           char bokstav = 'a';
+           bool falseortrue = true; */
+
+            int a = 5;
+            decimal b = 3.0M;
+
+            var result = a + b;
+
+            Console.WriteLine($"Resultatet er {result}");
+           
+           
         }
     }
 }
